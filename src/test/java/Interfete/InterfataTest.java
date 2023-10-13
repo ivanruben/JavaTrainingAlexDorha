831ae05ffb01d428624c782a499fd29436d18696
ivanruben <ben_ru_ben@yahoo.com>
2023-10-13 15:08:38 +0300

    Am adaugat static methode

--- src/test/java/Interfete/InterfataTest.java
+++ src/test/java/Interfete/InterfataTest.java
@@ -12,6 +12,7 @@ PersoanaStudent persoanaStudent = new PersoanaStudent("Gigi","Craca",55 );
 PersoanaStudentAngajat persoanaStudentAngajat = new PersoanaStudentAngajat("Jibli","Toru",25 );
 
 persoanaAngajat.munceste();
+new
 persoanaStudent.invata();
 persoanaStudentAngajat.invata();
 persoanaStudentAngajat.munceste();
